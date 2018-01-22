58f9c4a158d4db980cac216125c18ccaa344e112
Julien <julien.jourdain@thalesgroup.com>
2018-01-22 15:40:58 +0100

    Add comments to python script and update README file.

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Ansible_LinuxUserAccountsManagement
+# Manage linux users with Ansible
+
+## Prerequisites
+
+This set of playbooks and scripts needs you to use the "users" YAML dictionary in the `params.yml` file, which will be used to manage users on one or more linux servers.
+
+Here is a pretty simple example for dictionary fill :
+
+```YAML
+users:
+- name: foo
+  email: foo@foo.bar
+  shell: /bin/bash
+  state: present
+```
+
+## How to use the set
+
+```bash
+ansible-playbook global.yml -i inventories/production
+```
+Or
+```bash
+ansible-playbook global.yml -i inventories/production -u ssh_username -k -K
+ ```
--- scripts/generatePassword.py
+++ scripts/generatePassword.py
@@ -4,10 +4,12 @@ import random
 import string
 import yaml
 
+# Generate a random 16 characters password
 def generatePassword():
     clearPassword = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(16)])
     return clearPassword
 
+# Encrypt clear password
 def encryptPassword(password):
     encryptedPassword = crypt.crypt(password, '$6$' + 'salt1234')
     return encryptedPassword
@@ -16,20 +18,26 @@ with open("../ansible/params.yml", "r") as f:
     myDict = yaml.load(f)
 
 for user in myDict['users']:
+    # If keys exsists
     if 'clear_password' and 'encrypted_password' in user:
         password = user['clear_password']
+        # And password not empty
         if len(password) != 0:
+            # This means that the password already exists
             print("Password already exist, nothing to do !")
+        # Else, add password entries (clear and encrypted)
         else:
             clearPassword = generatePassword()
             encryptedPassword = encryptPassword(clearPassword)
             user['clear_password'] = clearPassword
             user['encrypted_password'] = encryptPassword
+    # Else, add password entries (clear and encrypted)
     else:
         clearPassword = generatePassword()
         encryptedPassword = encryptPassword(clearPassword)
         user['clear_password'] = clearPassword
         user['encrypted_password'] = encryptedPassword
 
+# Update YAML file
 with open("../ansible/params.yml", "w") as f:
     yaml.dump(myDict, f, default_flow_style=False)
